3799f9837b1bca64517a8219e0bb833e075bb336
tdybda17 <tdybda17@student.aau.dk>
2019-09-19 14:34:48 +0200

    ---

--- image_processing/image_process.py
+++ image_processing/image_process.py
@@ -6,22 +6,22 @@ from skimage.color import rgb2gray
 import glob
 from skimage.io import imread
 
-example_file = glob.glob('/Users/toby/PycharmProjects/P5/files/images/img1.png')[0]
+example_file = glob.glob('/Users/toby/PycharmProjects/P5/files/images/img2.jpg')[0]
 im = imread(example_file, as_gray=True)
 
-blobs_log = blob_log(im, max_sigma=30, num_sigma=10, threshold=0.1)
+blobs_log = blob_log(im, max_sigma=30, num_sigma=10, threshold=0.15)
 blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
 num_rows = len(blobs_log)
-print(num_rows)
 
 # showing the image
 fig, ax = plt.subplots(1, 1)
 plt.imshow(im, cmap=cm.gray)
+i = 0
 for blob in blobs_log:
+    i += 1
     y, x, r = blob
     c = plt.Circle((x, y), r + 5, color='blue', linewidth=1, fill=False)
-    print(c)
-    ax.add_patch(c)
+    # ax.add_patch(c)
 
 plt.show()
 
