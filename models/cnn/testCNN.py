918ede056c2be44ea9a9e3a292f8d7c48ff020f8
tdybda17 <tdybda17@student.aau.dk>
2019-10-01 12:49:31 +0200

    efwefwegwegweg wefwefwefwef rwgergerger

--- models/cnn/testCNN.py
+++ models/cnn/testCNN.py
@@ -10,54 +10,26 @@ classifier = load_model('model.h5')
 
 from keras.preprocessing import image
 
-# Test cardboard image
+# Category options
+categories = {
+    0: 'cardboard',
+    1: 'glass',
+    2: 'metal',
+    3: 'paper',
+    4: 'plastic'
+}
 
-test_image = image.load_img('testIMG/cardboard281.jpg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-if 1 in result[:, 0]:
-    print("This was correctly cardboard")
-else:
-    print("This was incorrectly predicted")
 
-# Test glass image
-test_image = image.load_img('testIMG/glass326.jpg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-if 1 in result[:, 1]:
-    print("This was correctly glass")
-else:
-    print("This was incorrectly predicted")
+def get_result(result_arr, categories_arr):
+    for res in result_arr:
+        return categories_arr[res]
 
-# Test metal image
-test_image = image.load_img('testIMG/metal333.jpg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-if 1 in result[:, 2]:
-    print("This was correctly metal")
-else:
-    print("This was incorrectly predicted")
 
-# Test paper image
-test_image = image.load_img('testIMG/paper223.jpg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-if 1 in result[:, 3]:
-    print("This was correctly paper")
-else:
-    print("This was incorrectly predicted")
-
-# Test plastic image
 test_image = image.load_img('testIMG/plastic313.jpg', target_size=(64, 64))
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
+test_image = np.expand_dims(test_image, axis=0)
 result = classifier.predict(test_image)
-if 1 in result[:, 4]:
-    print("This was correctly plastic")
-else:
-    print("This was incorrectly predicted")
+
+print(get_result(result, categories))
+
 
---
+++ models/test_file.py
@@ -0,0 +1,7 @@
+categories = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three'
+}
+
+print(categories[1])
