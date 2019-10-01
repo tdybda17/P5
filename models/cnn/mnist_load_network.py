eafd805297d74af7fb78ac03ccf5dd55751bc606
tdybda17 <tdybda17@student.aau.dk>
2019-10-01 14:34:55 +0200

    handwriting_test

--- models/cnn/mnist_load_network.py
+++ models/cnn/mnist_load_network.py
@@ -3,13 +3,15 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.backend import reshape
 from keras.models import load_model
 import numpy as np
 from keras.preprocessing import image
 
 classifier = load_model('mnist-trained-network.h5')
 
-test_image = image.load_img('testIMG/handwriting_img/2-tal.png', target_size=(28, 28, 1))
+# Load in image
+test_image = reshape(image.load_img('testIMG/handwriting_img/2-tal.png', target_size=(28, 28)), shape=(28, 28, 1))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = classifier.predict(test_image)
