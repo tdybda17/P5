832807600919d52e94d087e02750451fc97ddd5b
tdybda17 <tdybda17@student.aau.dk>
2019-10-08 11:14:12 +0200

    handwriting_test

--- models/cnn/mnist_load_network.py
+++ models/cnn/mnist_load_network.py
@@ -7,15 +7,15 @@ from keras.models import load_model
 import numpy as np
 from keras.preprocessing import image
 
-classifier = load_model('mnist-trained-network.h5')
+classifier = load_model('binaryModel.h5')
 
 # Load in image
 image_paths = [
-    '4.jpg', '19.jpg', '7050.jpg'
+    'can22.jpg', 'can_23.jpg', 'can22.jpg'
 ]
 
 for path in image_paths:
-    test_image = image.load_img('testIMG/handwriting_img/' + path, target_size=(28, 28), color_mode='grayscale')
+    test_image = image.load_img('../../files/images/test_set/waste/cans/' + path, target_size=(28, 28), color_mode='grayscale')
     test_image = image.img_to_array(test_image)
     test_image = np.expand_dims(test_image, axis=0)
     result = classifier.predict_proba(test_image)
