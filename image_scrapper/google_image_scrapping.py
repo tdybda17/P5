dc15379052e148b61f07a80659edb56cfb00ef12
tdybda17 <tdybda17@student.aau.dk>
2019-10-11 08:53:41 +0200

    handwriting_test

--- image_scrapper/google_image_scrapping.py
+++ image_scrapper/google_image_scrapping.py
@@ -8,9 +8,9 @@ from image_scrapper.categories import categories
 BATCH_SIZE = 2000
 GOOGLE_IMAGE_SIZE = 'medium'
 
-google_crawler = GoogleImageCrawler(storage={'root_dir': '/Volumes/BOOT/sports-car'}, downloader_threads=80, feeder_threads=20, parser_threads=20)
+google_crawler = GoogleImageCrawler(storage={'root_dir': '/Volumes/BOOT/others10'}, downloader_threads=80, feeder_threads=20, parser_threads=20)
 filter = dict(size=GOOGLE_IMAGE_SIZE)
-google_crawler.crawl(keyword='sports car', filters=filter, max_num=BATCH_SIZE)
+google_crawler.crawl(keyword='wine bottles', filters=filter, max_num=BATCH_SIZE)
 
 
 """
--- image_scrapper/image_compressor.py
+++ image_scrapper/image_compressor.py
@@ -4,22 +4,23 @@ from resizeimage import resizeimage
 
 import os
 
-path = '/Volumes/BOOT/sports-car/'
+for i in range(10):
+    path = '/Volumes/BOOT/others' + str(i) + '/'
 
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    if '/Volumes/BOOT/.Trashes' not in r:
-        for file in f:
-            if '.jpg' in file:
-                files.append(os.path.join(r, file))
+    files = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        if '/Volumes/BOOT/.Trashes' not in r:
+            for file in f:
+                if '.jpg' in file:
+                    files.append(os.path.join(r, file))
 
-i = 0
-for file in files:
-    with open(file, 'r+b') as f:
-        with Image.open(f) as image:
-            cover = resizeimage.resize_cover(image, [200, 200])
-            filename = '/Volumes/BOOT/sports-car-200x200/car_1_' + str(i) + '.jpg'
-            cover.save(filename, image.format)
-            i = i + 1
-            print(filename + ' was saved')
+    j = 0
+    for file in files:
+        with open(file, 'r+b') as f:
+            with Image.open(f) as image:
+                cover = resizeimage.resize_cover(image, [200, 200])
+                filename = '/Volumes/BOOT/other_waste/w_' + str(i) + '_' + str(j) + '.jpg'
+                cover.save(filename, image.format)
+                j = j + 1
+                print(filename + ' was saved')
