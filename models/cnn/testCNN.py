3c79618a383c35d85bcacb9fc2e1b285edacc0e8
jaxels10 <jaxels10@student.aau.dk>
2019-10-17 09:21:37 +0200

    Slettet lort

--- models/cnn/testCNN.py
+++ models/cnn/testCNN.py
@@ -1,13 +1,13 @@
 from keras.models import load_model
 import numpy as np
 
-classifier = load_model('tmodel.h5')
+classifier = load_model('categoricalModel.h5')
 
 from keras.preprocessing import image
 
 # Test cardboard image
-
-test_image = image.load_img('testIMG/petercan1.jpg', target_size=(64, 64))
+np.set_printoptions(suppress=True)
+test_image = image.load_img('testIMG/plastic.jpg', target_size=(64, 64))
 test_image = image.img_to_array(test_image)
 test_image = test_image/255
 test_image = np.expand_dims(test_image, axis = 0)
