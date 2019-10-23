d581be061976af6d388c5e5aaa096d1351b31ba9
jaxels10 <jaxels10@student.aau.dk>
2019-10-23 14:21:31 +0200

    Jev ny

--- models/cnn/cnn_tests/cnn_multi.py
+++ models/cnn/cnn_tests/cnn_multi.py
@@ -1,14 +1,3 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
@@ -18,8 +7,7 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras import backend as K
-import matplotlib.pyplot as plt
-import models.cnn.cnn_tests.customfunctions
+from models.cnn.cnn_tests.customfunctions import getinitconvlayer, getconvlayer, getmaxpoollayer, getdropoutlayer, getdenselayer, createplot
 
 # Initialising the CNN with the sequential model
 classifier = Sequential()
@@ -27,21 +15,21 @@ classifier = Sequential()
 # Step 1 - Convolution. Add a Convolution2D layer with 32 filters, 3x3 kernel size, 3 stride,
 # input shape of image should be 64x64x3 and the activation function is relu, which makes all negative
 # values in the matrix to zero.
-classifier.add(Convolution2D(32, 3, 3, input_shape = (200, 112, 3), activation = 'relu'))
+classifier.add(getinitconvlayer(32, 3, 3))
 
 # Step 2 - Pooling. Adds a pooling layer with maxpooling, which only saves the max value into the
 # new matrix
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(getmaxpoollayer(2))
 
 # Adding a second convolutional layer
-classifier.add(Convolution2D(32, 3, 3, activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(getconvlayer(32, 3, 3))
+classifier.add(getmaxpoollayer(2))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(activation="relu", units=128))
+classifier.add(getdenselayer(128))
 classifier.add(Dense(activation="softmax", units=5))
 
 # Compiling the CNN
@@ -69,31 +57,12 @@ test_set = test_datagen.flow_from_directory('../../files/images/dataset-resized/
                                             class_mode = 'categorical')
 
 history = classifier.fit_generator(training_set,
-                         samples_per_epoch = 8000,
-                         nb_epoch = 3,
+                         samples_per_epoch = 100, #var 8000
+                         nb_epoch = 1,
                          validation_data = test_set,
                          nb_val_samples = 2000)
 
-history_dict = history.history
-history_dict.keys()
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1, len(acc) + 1)
-plt.clf()
-# "bo" is for "blue dot"
-plt.plot(epochs, loss, 'bo', label='Training loss')
-# b is for "solid blue line"
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
+createplot(history)
 
 
 # classifier.save('categoricalModel.h5')
