b64ef1eb9964752b8dc5fb50aacf4245ba36b02d
jaxels10 <jaxels10@student.aau.dk>
2019-10-23 12:32:05 +0200

    Jev ny

--- models/cnn/cnn_tests/functions.py
+++
@@ -1,9 +0,0 @@
-from keras.layers import Convolution2D
-
-
-
-def getinitconvlayer(filters, kernel, stride):
-    return Convolution2D(filters, kernel, stride, input_shape = (200, 112, 3), activation='relu')
-
-def getconvlayer(filters, kernel, stride):
-    return Convolution2D(filters, kernel, stride, activation='relu')
