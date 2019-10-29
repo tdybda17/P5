b59fa7716f77fab1f30f9a48705056f9729305e7
tdybda17 <tdybda17@student.aau.dk>
2019-10-29 10:27:12 +0100

    handwriting_test

--- image_compressor/compress_images.py
+++ image_compressor/compress_images.py
@@ -2,10 +2,10 @@ from image_compressor.dir_walker.dir_walker import walk_dir
 from image_compressor.image_comp import compress_image
 
 
-source = '/Users/toby/Downloads/cans_1920'
+source = '/Users/toby/Desktop/AllTestPictures/PlastikTest'
 files = walk_dir(path=source, files_extensions=['.jpg', '.png', '.jpeg'])
 i = 0
 for file in files:
-    filename = '/Users/toby/Desktop/cans/can_' + str(i) + '_200x112.jpg'
+    filename = '/Users/toby/Desktop/TestBilleder/TestPlastik/plastic_' + str(i) + '_200x112.jpg'
     compress_image(file, dest_filename=filename, size=[200, 112])
     i = i + 1
