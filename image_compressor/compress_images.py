2ddeec1a12a5f0961971f1ee97dbc1f747572d01
tdybda17 <tdybda17@student.aau.dk>
2019-10-30 12:20:52 +0100

    -Image compresser merged into this

--- image_compressor/compress_images.py
+++ image_compressor/compress_images.py
@@ -3,7 +3,7 @@ from image_compressor.image_comp import compress_image
 
 __basedir__ = '/Users/toby/Desktop/'
 # [0] should not end with '/' and [1] is the name of the source
-source_options = ['AllTestPictures/PlastikTest', 'plastic']
+source_options = ['NyeBilleder/BatteriTest', 'battery']
 
 source = __basedir__ + source_options[0]
 files = walk_dir(path=source, files_extensions=['.jpg', '.png', '.jpeg'])
@@ -11,4 +11,5 @@ i = 0
 for file in files:
     filename = __basedir__ + 'test_' + source_options[1] + '/' + source_options[1] + '_' + str(i) + '_200x112.jpg'
     compress_image(file, dest_filename=filename, size=[200, 112])
+    print(filename + ' was created')
     i = i + 1
