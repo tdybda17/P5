0191279b1cfc974e4c3aba77e862539ac6299213
Dunse <dgrege17@student.aau.dk>
2019-10-31 14:59:06 +0100

    Flere funktioner tilføjet

--- Ev3/Ev3Test.py
+++ Ev3/Ev3Test.py
@@ -1,13 +1,19 @@
+import sys
 import rpyc
 
 conn = rpyc.classic.connect('ev3dev')
 ev3_motor = conn.modules['ev3dev2.motor']
 ev3_sensor = conn.modules['ev3dev2.sensor.lego']
+ev3_os = conn.modules['os']
+ev3_time = conn.modules['time']
+ev3_sys = conn.modules['sys']
+ev3_display = conn.modules['ev3dev2.display']
 cv2 = conn.modules['cv2']
 print('Ev3 imports done')
 
 webcam = cv2.VideoCapture(0)
-print(cv2.__version__)
+print("cv2 version: " + cv2.__version__)
+lcd = ev3_display.Display()
 
 def take_picture() :
     for x in range(4):
@@ -23,7 +29,4 @@ def initialize_and_run_motors() :
     m1.on(-30)
     m2.on(-30)
 
-
-
-
-
+take_picture()
