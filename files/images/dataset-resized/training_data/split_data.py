f7f8a6d0f348a849118f1ce024c37acb3f93975c
TuanAnh <avu15@student.aau.dk>
2019-11-05 11:29:05 +0100

    flyttet på billederne

--- files/images/dataset-resized/training_data/split_data.py
+++ files/images/dataset-resized/training_data/split_data.py
@@ -1,14 +1,14 @@
 import os
 import random
 
-files = os.listdir('Glas/')
+files = os.listdir('Batterier/')
 random.shuffle(files)
 length = len(files)
-test_size = length * 0.16
+test_size = length * 0.33
 
 i = 0
 for file in files:
-    os.rename("Glas/" + file, "../test_data/Glas/" + file)
+    os.rename("Batterier/" + file, "../test_data/Batterier/" + file)
     i = i + 1
     if i > test_size:
         break
