7d59db8a8c41afd784152143ea8adc1b1faf7147
Dunse <dgrege17@student.aau.dk>
2019-11-05 14:14:23 +0100

    Nyt

--- Ev3/Ev3Test.py
+++ Ev3/Ev3Test.py
@@ -22,7 +22,7 @@ display = ev3_display.Display()
 
 print('initializations done')
 
-
+#Skal bruges til at hente billedet fra Ev3'en
 def create_ssh_client(server, port, user, password) :
     client = SSHClient()
     client.load_system_host_keys()
@@ -30,7 +30,7 @@ def create_ssh_client(server, port, user, password) :
     client.connect(server, port, user, password)
     return client
 
-
+#Henter billedet fra Ev3'en via en ssh forbindelse og ligger filen ind i mappen pictures i PyCharm
 def get_picture() :
     ssh = create_ssh_client("ev3dev", "22", "robot", "maker")
     scp = SCPClient(ssh.get_transport())
@@ -39,16 +39,19 @@ def get_picture() :
     scp.close()
     print("picture saved")
 
-
+#Initializer og retunere de 2 motorer til båndet
 def initialize_motors() :
     m1 = ev3_motor.LargeMotor('outA')
     m2 = ev3_motor.LargeMotor('outB')
     return m1, m2
 
+#initializer den infarøde sensor og retunere den samt dens første læste værdi
 def initialize_inf() :
     inf = ev3_sensor.InfraredSensor()
-    return inf
+    start_value = inf.proximity
+    return inf, start_value
 
+#Tager et billede og gemmer det på Ev3'en derefter bliver det flyttet over til computeren
 def take_picture() :
     for x in range(4):
         check = webcam.grab()
@@ -68,16 +71,29 @@ def stop_motors(m1, m2) :
     m1.on(0)
     m2.on(0)
 
-
+#Opdaterer Ev3'ens skærm med den angivne tekst
 def write_to_screen(text) :
     display.text_pixels(text, 120, 50, True, 'black', font=ev3_fonts.load('helvB24'))
     display.update()
+
+#Checker hvis der er noget indenfor sensoren hvis der er bliver der retuneret true
+def inf_check_for_object(inf, start_value) :
+    distance_buffer = 4
+    distance_now = inf.proximity
+    if distance_now < (start_value - distance_buffer) :
+        write_to_screen('Der er et object ' + str(inf.proximity))
+        return True
+    else :
+        write_to_screen('Der er ikke et object ' + str(inf.proximity))
+        return False
 
 def main() :
     print("cv2 version: " + cv2.__version__)
 
     #m1, m2 = initialize_motors()
-    #inf = initialize_inf()
+    inf, start_value = initialize_inf()
+
+
 
     # run_motors(m1, m2, 0)
     take_picture()
