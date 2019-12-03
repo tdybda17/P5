0cd6f0e76d3c549fbf067bcb9fb4015c7160daed
TuanAnh <avu15@student.aau.dk>
2019-12-03 15:43:55 +0100

    test model

--- models/cnn/CNN_model_test/cnn_27.py
+++ models/cnn/CNN_model_test/cnn_27.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Convolution2D, Dropout
+from keras.layers import Convolution2D, Dropout, Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -8,26 +8,34 @@ from models.cnn.cnn_tests.customfunctions import get_init_conv_layer, get_conv_l
     get_maxpool_layer, get_dropout_layer, get_dense_layer, create_plot_acc, create_plot_loss, get_fit_generator, \
     get_train_data_gen, get_test_data_gen, get_image_data_gen, get_rescale_gen
 
-graph_acc_name = "cnn_9_acc"
-graph_loss_name = "cnn_9_loss"
-model_name = "cnn_9.h5"
+graph_acc_name = "cnn_27_acc"
+graph_loss_name = "cnn_27_loss"
+model_name = "cnn_27.h5"
+kernel = 3
+stride = 1
+image_size_x = 150
+image_size_y = 150
+activation = 'elu'
+
 
 classifier = Sequential()
 
-classifier.add(get_init_conv_layer(64, 3, 1))
+classifier.add(Conv2D(filters=64, kernel_size=kernel, strides=stride, input_shape=(image_size_y, image_size_x, 3),
+                      activation=activation))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(128, 3, 1))
+classifier.add(Conv2D(filters=128, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(256, 3, 1))
+classifier.add(Conv2D(filters=256, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(512, 3, 1))
+classifier.add(Conv2D(filters=512, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(512, 3, 1))
+classifier.add(Conv2D(filters=512, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
+
 
 classifier.add(Dropout(0.5))
 classifier.add(Flatten())
@@ -50,5 +58,5 @@ history = get_fit_generator(classifier, training_set, test_set)
 create_plot_acc(history, graph_acc_name)
 create_plot_loss(history, graph_loss_name)
 
-classifier.save(model_name)
+# classifier.save(model_name)
 K.clear_session()
--- models/cnn/CNN_model_test/cnn_28.py
+++ models/cnn/CNN_model_test/cnn_28.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Convolution2D, Dropout
+from keras.layers import Convolution2D, Dropout, Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
@@ -8,25 +8,31 @@ from models.cnn.cnn_tests.customfunctions import get_init_conv_layer, get_conv_l
     get_maxpool_layer, get_dropout_layer, get_dense_layer, create_plot_acc, create_plot_loss, get_fit_generator, \
     get_train_data_gen, get_test_data_gen, get_image_data_gen, get_rescale_gen
 
-graph_acc_name = "cnn_9_acc"
-graph_loss_name = "cnn_9_loss"
-model_name = "cnn_9.h5"
+graph_acc_name = "cnn_28_acc"
+graph_loss_name = "cnn_28_loss"
+model_name = "cnn_28.h5"
+kernel = 3
+stride = 1
+image_size_x = 150
+image_size_y = 150
+activation = 'tanh'
 
 classifier = Sequential()
 
-classifier.add(get_init_conv_layer(64, 3, 1))
+classifier.add(Conv2D(filters=64, kernel_size=kernel, strides=stride, input_shape=(image_size_y, image_size_x, 3),
+                      activation=activation))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(128, 3, 1))
+classifier.add(Conv2D(filters=128, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(256, 3, 1))
+classifier.add(Conv2D(filters=256, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(512, 3, 1))
+classifier.add(Conv2D(filters=512, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
-classifier.add(get_conv_layer(512, 3, 1))
+classifier.add(Conv2D(filters=512, kernel_size=kernel, strides=stride, activation=activation, padding='same'))
 classifier.add(get_maxpool_layer(2))
 
 classifier.add(Dropout(0.5))
@@ -50,5 +56,5 @@ history = get_fit_generator(classifier, training_set, test_set)
 create_plot_acc(history, graph_acc_name)
 create_plot_loss(history, graph_loss_name)
 
-classifier.save(model_name)
+# classifier.save(model_name)
 K.clear_session()
