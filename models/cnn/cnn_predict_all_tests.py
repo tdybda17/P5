591e87add2bc452e93fa38638c815f8866117ed8
jaxels10 <jaxels10@student.aau.dk>
2019-12-17 14:20:39 +0100

    Commented

--- models/cnn/cnn_predict_all_tests.py
+++ models/cnn/cnn_predict_all_tests.py
@@ -7,8 +7,8 @@ classifier = load_model('../cnn.h5')
 
 resultarray = [0, 0, 0]
 
-for filename in os.listdir('../../images/'):
-     img = cv2.imread('../../images/Batteries' + filename, cv2.IMREAD_COLOR)
+for filename in os.listdir('../../images/test_set_cropped'):
+     img = cv2.imread('../../images/test_set_cropped/Batteries' + filename, cv2.IMREAD_COLOR)
      test_image = cv2.resize(img, (150, 150), interpolation=cv2.INTER_AREA)
      test_image = test_image / 255
      test_image = np.expand_dims(test_image, axis=0)
@@ -21,8 +21,8 @@ print('Batteries: ')
 print(resultarray)
 
 resultarray = [0, 0, 0]
-for filename in os.listdir('../../images/'):
-     img = cv2.imread('../../images/Cans' + filename, cv2.IMREAD_COLOR)
+for filename in os.listdir('../../images/test_set_cropped'):
+     img = cv2.imread('../../images/test_set_cropped/Cans' + filename, cv2.IMREAD_COLOR)
      test_image = cv2.resize(img, (150, 150), interpolation=cv2.INTER_AREA)
      test_image = test_image / 255
      test_image = np.expand_dims(test_image, axis=0)
@@ -35,8 +35,8 @@ print('Cans: ')
 print(resultarray)
 
 resultarray = [0, 0, 0]
-for filename in os.listdir('../../images/'):
-     img = cv2.imread('../../images/Glas' + filename, cv2.IMREAD_COLOR)
+for filename in os.listdir('../../images/test_set_cropped'):
+     img = cv2.imread('../../images/test_set_cropped/Glas' + filename, cv2.IMREAD_COLOR)
      test_image = cv2.resize(img, (150, 150), interpolation=cv2.INTER_AREA)
      test_image = test_image / 255
      test_image = np.expand_dims(test_image, axis=0)
